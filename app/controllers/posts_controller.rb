95cc1c36a8b9762224ea0f489c227cd627f36e2d
Haider Zaman <haider.zaman@coeus-solutions.de>
2021-05-12 21:13:18 +0500

    comment added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
-
+#posts
   # GET /posts or /posts.json
   def index
     @posts = Post.all
